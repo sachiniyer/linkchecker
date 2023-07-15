24f554d3d11189992c0731c3791068e8c6239aad
Sachin Iyer <sachinjiyer@gmail.com>
2023-07-14 18:34:44 -0700

    change to all my sites

--- tests/test.rs
+++ tests/test.rs
@@ -4,20 +4,20 @@ mod tests {
 
     #[test]
     fn test_application() {
-        let args = vec!["https://yatin.cc"];
+        let args = vec!["https://sachiniyer.com"];
         Assert::main_binary()
             .with_args(&args)
             .stdout()
-            .is("https://yatin.cc: 200 OK")
+            .is("https://sachiniyer.com: 200 OK")
             .unwrap()
     }
 
     #[test]
     fn test_stdin() {
         Assert::main_binary()
-            .stdin("https://yatin.cc")
+            .stdin("https://sachiniyer.com")
             .stdout()
-            .is("https://yatin.cc: 200 OK")
+            .is("https://sachiniyer.com: 200 OK")
             .unwrap()
     }
 
@@ -26,17 +26,17 @@ mod tests {
         Assert::main_binary()
             .with_args(&["-i", "tests/test.txt"])
             .stdout()
-            .is("https://yatin.cc: 200 OK\nhttps://yatin.cc: 200 OK")
+            .is("https://sachiniyer.com: 200 OK\nhttps://sachiniyer.com: 200 OK")
             .unwrap()
     }
 
     #[test]
     fn test_redirect() {
-        let args = vec!["https://dev.hpc.nyu.edu/corelink/corelink-examples"];
+        let args = vec!["https://ai.sachiniyer.com"];
         Assert::main_binary()
             .with_args(&args)
             .stdout()
-            .is("https://dev.hpc.nyu.edu/corelink/corelink-examples: 200 OK")
+            .is("https://ai.sachiniyer.com: 200 OK")
             .unwrap()
     }
 }
